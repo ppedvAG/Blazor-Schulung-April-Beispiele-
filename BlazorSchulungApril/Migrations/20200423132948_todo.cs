b05189b38c9b401c5b1d7d0fbb9160b734f37fff
Hannes Preishuber <hannesp@ppedv.de>
2020-04-23 15:31:20 +0200

    Create DB from dbcontext

---
+++ BlazorSchulungApril/Migrations/20200423132948_todo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorSchulungApril.Migrations
+{
+    public partial class todo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aufgabe",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(nullable: true),
+                    ErledigtBis = table.Column<DateTime>(nullable: false),
+                    Erledigt = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aufgabe", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aufgabe");
+        }
+    }
+}
