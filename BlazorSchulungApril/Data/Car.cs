356a147228d199dfb7fdfed03db043c7e968e6fb
Hannes Preishuber <hannesp@ppedv.de>
2020-04-24 10:40:32 +0200

    Car Model Data Annotation

---
+++ BlazorSchulungApril/Data/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSchulungApril.Data
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        [Range(42,450,ErrorMessage ="PS Range beachten")]
+        public int Leistung { get; set; }
+        [Required(ErrorMessage ="da muss name rein")]
+        public string Hersteller { get; set; }
+    }
+}
